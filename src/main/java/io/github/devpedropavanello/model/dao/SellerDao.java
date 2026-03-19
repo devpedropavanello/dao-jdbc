51302bad6e2276f423591c04f3cf398a0124c17e
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-03-18 23:14:45 -0300

    feat: add SellerDaoJDBC findByDepartment method

--- src/main/java/io/github/devpedropavanello/model/dao/SellerDao.java
+++ src/main/java/io/github/devpedropavanello/model/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package io.github.devpedropavanello.model.dao;
 
+import io.github.devpedropavanello.model.entities.Department;
 import io.github.devpedropavanello.model.entities.Seller;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface SellerDao {
     void deleteById(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
 }
